1aed465e07edb862fedb6246f84409c0ed49e8d5
marcinz99 <44765764+marcinz99@users.noreply.github.com>
2019-03-24 14:56:34 +0100

    Update asd002_kolejka_priorytetowa.cpp

--- asd002_kolejka_priorytetowa.cpp
+++ asd002_kolejka_priorytetowa.cpp
@@ -1,115 +1,92 @@
 #include <iostream>
-#define hsize(n) n.t[0]
+#define hsize(n) (*n).t[0]
 using namespace std;
 
 struct kopiec{
     int* t;
-    const int N = 0;
-    kopiec(){};
-    kopiec(int a[], int s) : N(s){
+    const int N;
+    kopiec(): N(0) {};
+    kopiec(int a[], int s): N(s) {
         t = new int[s+1];
         t[0] = s;
-        for(int i=1; i<=s; i++){
-            t[i] = a[i-1];
+        for(int i=0; i<s; i++){
+            t[i+1] = a[i];
         }
-    }
-};
-
-struct kolejka{
-    kopiec kol;
-    kolejka(){};
-    kolejka(int t[], int s) : kol(kopiec(t, s)){};
+    };
 };
 
 int parent(int i){return i/2;}
 int left(int i){return 2*i;}
 int right(int i){return 2*i+1;}
 
-void Wypisz(kopiec heap){
-    cout << "Elementy kopca: ";
+void Wypisz(kopiec* heap){
+    cout << "Elementy: ";
     for(int i=1; i<=hsize(heap); i++){
-        cout << heap.t[i] << ", ";
+        cout << heap->t[i] << ", ";
     }cout << "\n";
 }
 
-void Wypisz(kolejka que){
-    cout << "Elementy kopca-kolejki: ";
-    for(int i=1; i<=hsize(que.kol); i++){
-        cout << que.kol.t[i] << ", ";
-    }cout << "\n";
-}
-
-void Heapify(kopiec heap, int i){
+void Heapify(kopiec* &heap, int i){
     if(i<=0 || i>hsize(heap)) return;
     int maxm = i;
     int p = left(i);
     int r = right(i);
 
-    if(p<=hsize(heap) && heap.t[p]>heap.t[maxm]){
-        maxm = p;
-    }
-    if(r<=hsize(heap) && heap.t[r]>heap.t[maxm]){
-        maxm = r;
-    }
+    if(p<=hsize(heap) && heap->t[p]>(heap->t[maxm])) maxm = p;
+    if(r<=hsize(heap) && heap->t[r]>(heap->t[maxm])) maxm = r;
     if(maxm != i){
-        heap.t[maxm]^=heap.t[i]^=heap.t[maxm]^=heap.t[i];
+        heap->t[maxm]^=heap->t[i]^=heap->t[maxm]^=heap->t[i];
         Heapify(heap, maxm);
     }
 }
 
-kopiec BuildHeap(int t[], int s){
-    kopiec heap(t, s);
+void BuildHeap(kopiec* &heap, int t[], int s){
+    heap = nullptr;
+    heap = new kopiec(t, s);
     for(int i=s/2; i>0; i--){
         Heapify(heap, i);
     }
-    return heap;
 }
 
-int GetMax(kopiec heap){
+int GetMax(kopiec* &heap){
     if(hsize(heap)==0) return -1;
-    int result = heap.t[1];
 
-    heap.t[1] = heap.t[hsize(heap)];
+    int result = heap->t[1];
+    heap->t[1] = heap->t[hsize(heap)];
     hsize(heap)--;
     Heapify(heap, 1);
     return result;
 }
 
-void Put(kopiec heap, int a){
-    if(hsize(heap)==heap.N) return;
+void Insert(kopiec* &heap, int a){
+    if(hsize(heap)==heap->N) return;
+
     hsize(heap)++;
-    heap.t[hsize(heap)] = a;
-
-    int i = hsize(heap);
-    while(i>1 && heap.t[i]>heap.t[parent(i)]){
-        heap.t[i]^=heap.t[parent(i)]^=heap.t[i]^=heap.t[parent(i)];
-        i = parent(i);
+    heap->t[hsize(heap)] = a;
+    for(int i=hsize(heap); i>1; i=parent(i)){
+        if(heap->t[i] > heap->t[parent(i)]){
+            heap->t[parent(i)]^=heap->t[i]^=heap->t[parent(i)]^=heap->t[i];
+        }
     }
 }
 
-kolejka BuildKol(int t[], int s){
-    kolejka que(t, s);
-    for(int i=s/2; i>0; i--){
-        Heapify(que.kol, i);
-    }
-    return que;
-}
-void GetMax(kolejka que){GetMax(que.kol);}
-void Put(kolejka que, int a){Put(que.kol, a);}
+struct kolejka{
+    kopiec* kol = nullptr;
+};
+void Wypisz(kolejka que){Wypisz(que.kol);}
+void BuildKol(kolejka &que, int t[], int s){BuildHeap(que.kol, t, s);}
+int GetMax(kolejka &que){return GetMax(que.kol);}
+void Insert(kolejka &que, int a){Insert(que.kol, a);}
 
 int main(){
-    int t[] = {1,2,3,4,5};
-    kopiec heap = BuildHeap(t, 5);
-    Wypisz(heap);
-    GetMax(heap);
-    Wypisz(heap);
-    Put(heap, 3);
-    Wypisz(heap);
-    cout << "\n\n";
-    kolejka que = BuildKol(t, 5);
+    int t[] = {1,2,3,4,5,6,7,8,9,10};
+    kolejka que;
+    BuildKol(que, t, 10);
     Wypisz(que);
-    GetMax(que);
+    cout << "GetMax: " << GetMax(que) << "\n";
     Wypisz(que);
-    Put(que, 3);
+    int a = 8;
+    cout << "Insert: " << a << "\n";
+    Insert(que, a);
     Wypisz(que);
 }
