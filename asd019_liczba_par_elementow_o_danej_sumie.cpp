6ae8f89f466d8509f258430ff8e634b1be3bcd86
marcinz99 <44765764+marcinz99@users.noreply.github.com>
2019-04-03 12:02:50 +0200

    Update asd019_liczba_par_elementow_o_danej_sumie.cpp

--- asd019_liczba_par_elementow_o_danej_sumie.cpp
+++ asd019_liczba_par_elementow_o_danej_sumie.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 struct node{
     int value = 0;
+    int q;
     node* next = nullptr;
 };
 
@@ -35,14 +36,17 @@ int Hashing(int a, int mod){
     return a%mod;
 }
 
-bool IsThere(hashtab* &ht, int val){
+int HowMany(hashtab* &ht, int val){
     int shortcut = Hashing(val, ht->N);
     node* d = ht->first[shortcut];
 
     while(d != nullptr){
-        if(d->value == val) return true;
+        if(d->value == val){
+            d->q++;
+            return d->q;
         d = d->next;
     }
+    return 0;
 }
 
 void Add(hashtab* &ht, int val){
@@ -50,16 +54,18 @@ void Add(hashtab* &ht, int val){
     node* d = new node;
     d->value = val;
     d->next = ht->first[shortcut];
+    d->q = 0;
     ht->first[shortcut] = d;
 }
 
 int LiczbaSum(int t[], int n, int x){
     int licz = 0;
+    int tmp;
     hashtab* ht = new hashtab(n);
 
     for(int i=0; i<n; i++){
-        if(IsThere(ht, t[i])) licz++;
-        else Add(ht, x-t[i]);
+        licz += tmp = HowMany(ht, t[i]);
+        if(tmp == 0) Add(ht, x-t[i]);
     }
     delete ht;
     return licz;
